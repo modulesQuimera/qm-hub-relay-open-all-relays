f09ae0954b9bd0923a65661a1777783566e8a6cb
STRNEOH <strneoh@localhost.localdomain>
2020-07-21 16:36:20 -0300

    adding description

--- all_relays.js
+++ all_relays.js
@@ -4,7 +4,7 @@ module.exports = function(RED) {
 
     function all_relaysNode(config) {
         RED.nodes.createNode(this, config);
-
+        this.slot = config.slot;
         var node = this
 
         node.on('input', function(msg, send, done) {
@@ -14,17 +14,35 @@ module.exports = function(RED) {
             var exportMode = globalContext.get("exportMode");
             var currentMode = globalContext.get("currentMode");
             var command = {
-                    "type": "relay_modular_V1.0",
-                    "slot": 1,
-                    "compare": {},
-                    "method": "open_all_relays"
+                type: "relay_modular_V1_0",
+                slot: parseInt(node.slot),
+                compare: {},
+                method: "open_all_relays",
+                get_output: {},
             }
             var file = globalContext.get("exportFile")
             var slot = globalContext.get("slot");
-            if(currentMode == "test"){file.slots[slot].jig_test.push(command)}
-            else{file.slots[slot].jig_error.push(command)}
+            if(!(slot === "begin" || slot === "end")){
+                if(currentMode == "test"){
+                    file.slots[slot].jig_test.push(command);
+                }
+                else{
+                    file.slots[slot].jig_error.push(command);
+                }
+            }
+            else{
+                if(slot === "begin"){
+                    file.slots[0].jig_test.push(command);
+                    // file.begin.push(command);
+                }
+                else{
+                    file.slots[3].jig_test.push(command);
+                    // file.end.push(command);
+                }
+            }
             globalContext.set("exportFile", file);
              // node.status({fill:"green", shape:"dot", text:"done"}); // seta o status pra waiting
+            console.log(command)
             
             send(msg)
 
