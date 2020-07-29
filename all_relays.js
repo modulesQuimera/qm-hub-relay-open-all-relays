3e355c84f3e4fe6484ef978f066120d06820a2bb
Ian Alves <ian.alves21@gmail.com>
2020-07-29 19:47:00 -0400

    update

--- all_relays.js
+++ all_relays.js
@@ -1,15 +1,39 @@
 module.exports = function(RED) {
 
-    "use strict";
-
     function all_relaysNode(config) {
         RED.nodes.createNode(this, config);
         this.slot = config.slot;
-        var node = this
+
+        this.qtdAll = config.qtdAll;
+        this.slot_n=[]; 
+        this.slot_n.push(config.slot1);
+        this.slot_n.push(config.slot2) ;
+        this.slot_n.push(config.slot3) ;
+        this.slot_n.push(config.slot4) ;
+        this.slot_n.push(config.slot5) ;
+        this.slot_n.push(config.slot6) ;
+        this.slot_n.push(config.slot7) ;
+        this.slot_n.push(config.slot8) ;
+        this.slot_n.push(config.slot9) ;
+        this.slot_n.push(config.slot10) ;
+        this.slot_n.push(config.slot11) ;
+        this.slot_n.push(config.slot12) ;
+        this.slot_n.push(config.slot13) ;
+        this.slot_n.push(config.slot14) ;
+        this.slot_n.push(config.slot15) ;
+        this.slot_n.push(config.slot16) ;
+        this.slot_n.push(config.slot17) ;
+        this.slot_n.push(config.slot18) ;
+        this.slot_n.push(config.slot19) ;
+        this.slot_n.push(config.slot20) ;
+        this.slot_n.push(config.slot21) ;
+        this.slot_n.push(config.slot22) ;
+        this.slot_n.push(config.slot23) ;
+        this.slot_n.push(config.slot24) ;
+
+        var node = this;
 
         node.on('input', function(msg, send, done) {
-
-            // substitua a variavel msg pela a informação desejada a ser passada via serial
             var globalContext = node.context().global;
             var exportMode = globalContext.get("exportMode");
             var currentMode = globalContext.get("currentMode");
@@ -17,41 +41,75 @@ module.exports = function(RED) {
                 type: "relay_modular_V1_0",
                 slot: parseInt(node.slot),
                 compare: {},
-                method: "open_all_relays",
+                method: "default_relays",
                 get_output: {},
-            }
-            var file = globalContext.get("exportFile")
+            };
+            var file = globalContext.get("exportFile");
             var slot = globalContext.get("slot");
             if(!(slot === "begin" || slot === "end")){
                 if(currentMode == "test"){
                     file.slots[slot].jig_test.push(command);
+                    for(var j=0; j<node.qtdAll; j++){
+                        file.slots[slot].jig_test.push(
+                            {
+                                type: "relay_modular_V1_0",
+                                slot: parseInt(node.slot_n[j]),
+                                compare: {},
+                                method: "default_relays",
+                                get_output: {},
+                            }
+                        );
+                    }
                 }
                 else{
                     file.slots[slot].jig_error.push(command);
+                    for(var j=0; j<node.qtdAll; j++){
+                        file.slots[slot].jig_error.push(
+                            {
+                                type: "relay_modular_V1_0",
+                                slot: parseInt(node.slot_n[j]),
+                                compare: {},
+                                method: "open_all_relays",
+                                get_output: {},
+                            }
+                        );
+                    }
                 }
             }
             else{
                 if(slot === "begin"){
                     file.slots[0].jig_test.push(command);
-                    // file.begin.push(command);
+                    for(var j=0; j<node.qtdAll; j++){
+                        file.slots[0].jig_test.push(
+                            {
+                                type: "relay_modular_V1_0",
+                                slot: parseInt(node.slot_n[j]),
+                                compare: {},
+                                method: "open_all_relays",
+                                get_output: {},
+                            }
+                        );
+                    }
                 }
                 else{
                     file.slots[3].jig_test.push(command);
-                    // file.end.push(command);
+                    for(var j=0; j<node.qtdAll; j++){
+                        file.slots[3].jig_test.push(
+                            {
+                                type: "relay_modular_V1_0",
+                                slot: parseInt(node.slot_n[j]),
+                                compare: {},
+                                method: "open_all_relays",
+                                get_output: {},
+                            }
+                        );
+                    }
                 }
             }
             globalContext.set("exportFile", file);
-             // node.status({fill:"green", shape:"dot", text:"done"}); // seta o status pra waiting
-            console.log(command)
-            
-            send(msg)
-
+            console.log(command);
+            send(msg);
         });
-
-        // var nodeDaVariavel = config.NodeDaVariavel
-
     }
-
-    // nome do modulo
     RED.nodes.registerType("all_relays", all_relaysNode);
-}
+};
